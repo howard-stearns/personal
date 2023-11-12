16fe96ea7ebaac3a2f97a1b144fa007df24828dd
Howard (Aech Linden) Stearns <howard.stearns@gmail.com>
2023-11-12 15:44:42 -0800

    This time without syntax error

--- experiments/cloud.mjs
+++ experiments/cloud.mjs
@@ -1,5 +1,5 @@
 // Using some of the guts of Distributed Security because it's convenient for the authorization checks.
-impot Security from "https://kilroy-code.github.io/distributed-security/lib/security.mjs";
+import Security from "https://kilroy-code.github.io/distributed-security/lib/security.mjs";
 //import Security from "http://localhost:3000/@kilroy-code/distributed-security/lib/security.mjs";
 
 function path(resourceTag, ownerTag) {
